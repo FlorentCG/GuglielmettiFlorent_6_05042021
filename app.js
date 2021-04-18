3b71f9d514fae9bbc070a96284fc4a4a8ce422eb
Florent Guglielmetti <florent.guglielmetti@gmail.com>
2021-04-18 18:54:14 +0200

    Identification users

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 const express = require('express');
-const bodyParser = require('body-parser');
+/**const stuffRoutes = require('./routes/stuff');**/
 const app = express();
 const mongoose = require('mongoose');
-const Thing = require('./models/sauce');
+const userRoutes = require ('./routes/user');
 
 mongoose.connect('mongodb+srv://Flo:!anaflo!@cluster0.fubkl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
   { useNewUrlParser: true,
     useUnifiedTopology: true })
   .then(() => console.log('Connexion à MongoDB réussie !'))
   .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
@@ -17,32 +19,9 @@ app.use((req, res, next) => {
   next();
 });
 
-
-
 app.use(express.json());
-
-
-  app.post('/api/stuff', (req, res, next) => {
-    delete req.body._id;
-    const thing = new Thing({
-      ...req.body
-    });
-    thing.save()
-      .then(() => res.status(201).json({ message: 'Objet enregistré !'}))
-      .catch(error => res.status(400).json({ error }));
-  });
-  app.get('/api/stuff/:id', (req, res, next) => {
-    Thing.findOne({ _id: req.params.id })
-      .then(thing => res.status(200).json(thing))
-      .catch(error => res.status(404).json({ error }));
-  });
-
-
-  app.use('/api/stuff', (req, res, next) => {
-    Thing.find()
-      .then(things => res.status(200).json(things))
-      .catch(error => res.status(400).json({ error }));
-  });
+/**app.use('/api/stuff', stuffRoutes);**/
+app.use('/api/auth', userRoutes)
 
 
 
