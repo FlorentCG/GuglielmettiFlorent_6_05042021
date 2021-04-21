6fd0f86f355018718915a9220775c100250dbe99
Florent Guglielmetti <florent.guglielmetti@gmail.com>
2021-04-21 11:52:48 +0200

    Integration like/dislike

--- controllers/sauce.js
+++ controllers/sauce.js
@@ -12,20 +12,10 @@ exports.createSauce = (req,res,next) => {
         usersDisliked: [],
         imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
     });
-    if(!req.body.errorMessage) {
         sauce.save()
-        .then(() => { 
-            res.status(201).json({ message: 'La sauce a été créée avec succès!' }); 
-        })
-        .catch(error => { 
-            if(error.message.indexOf("to be unique")>0) {
-                req.body.errorMessage = "Le nom de cette sauce existe déjà!";
-            }
-            next();
-        })
-    } else {
-        next();
-    }
+        .then(() => res.status(201).json({ message: 'Sauce enregistrée !'}))
+    .catch(error => res.status(400).json({ error }));
+    
 };
 
 exports.getAllSauces = (req,res,next) => {
@@ -54,24 +44,10 @@ exports.modifyOneSauce = (req, res, next) => {
         ...JSON.parse(req.body.sauce),
         imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
     } : { ...req.body };
-    if(!req.body.errorMessage) {
+    
         Sauce.updateOne({ _id: req.params.id }, { ...sauceObject, _id: req.params.id })
-        .then(() => {
-            if(!req.file) {
-                res.status(200).json({ message: "La sauce a bien été modifiée!"})
-            } else {
-                next();
-            }
-        })
-        .catch(error => { 
-            if(error.message.indexOf("duplicate key")>0) {
-                req.body.errorMessage = "Le nom de cette sauce existe déjà!";
-            }
-            next();
-        })
-    } else {
-        next();
-    }
+        .then(() => res.status(200).json({ message: 'Sauce modifiée !'}))
+        .catch(error => res.status(400).json({ error }));
 };
 
 exports.deleteSauce = (req, res, next) => {
@@ -86,3 +62,125 @@ exports.deleteSauce = (req, res, next) => {
       })
       .catch(error => res.status(500).json({ error }));
 };
+
+exports.likeSauce = (req, res, next) => {
+    Sauce.findOne({ _id: req.params.id })
+    .then(sauce => {sauce.likes++; // Incriment likes
+      
+        sauce.save((err) => {
+            if (err) {
+                res.json({success: false, message: 'something went wrong'});
+            } else {
+                res.json({success: true, message: 'Sauce liked!'})
+            }
+    })})};
+   
+
+    exports.dislikeSauce = (req, res, next) => {
+        Sauce.findOne({ _id: req.params.id })
+        .then(sauce => {sauce.dislikes++; // Incriment likes
+          
+            sauce.save((err) => {
+                if (err) {
+                    res.json({success: false, message: 'something went wrong'});
+                } else {
+                    res.json({success: true, message: 'Sauce disliked!'})
+                }
+        })})};
+/**export class SingleSauceComponent implements OnInit {
+
+  loading: boolean;
+  sauce: Sauce;
+  userId: string;
+  likePending: boolean;
+  liked: boolean;
+  disliked: boolean;
+  errorMessage: string;
+
+  constructor(private sauces: SaucesService,
+              private route: ActivatedRoute,
+              private auth: AuthService,
+              private router: Router) { }
+
+  ngOnInit() {
+    this.userId = this.auth.getUserId();
+    this.loading = true;
+    this.route.params.subscribe(
+      (params) => {
+        this.sauces.getSauceById(params.id).then(
+          (sauce: Sauce) => {
+            this.sauce = sauce;
+            this.loading = false;
+            if (sauce.usersLiked.find(user => user === this.userId)) {
+              this.liked = true;
+            } else if (sauce.usersDisliked.find(user => user === this.userId)) {
+              this.disliked = true;
+            }
+          }
+        );
+      }
+    );
+    this.userId = this.auth.getUserId();
+  }
+
+  onLike() {
+    if (this.disliked) {
+      return 0;
+    }
+    this.likePending = true;
+    this.sauces.likeSauce(this.sauce._id, !this.liked).then(
+      (liked: boolean) => {
+        this.likePending = false;
+        this.liked = liked;
+        if (liked) {
+          this.sauce.likes++;
+        } else {
+          this.sauce.likes--;
+        }
+      }
+    );
+  }
+
+  onDislike() {
+    if (this.liked) {
+      return 0;
+    }
+    this.likePending = true;
+    this.sauces.dislikeSauce(this.sauce._id, !this.disliked).then(
+      (disliked: boolean) => {
+        this.likePending = false;
+        this.disliked = disliked;
+        if (disliked) {
+          this.sauce.dislikes++;
+        } else {
+          this.sauce.dislikes--;
+        }
+      }
+    );
+  }
+
+  onBack() {
+    this.router.navigate(['/sauces']);
+  }
+
+  onModify() {
+    this.router.navigate(['/modify-sauce', this.sauce._id]);
+  }
+
+  onDelete() {
+    this.loading = true;
+    this.sauces.deleteSauce(this.sauce._id).then(
+      (response: { message: string }) => {
+        console.log(response.message);
+        this.loading = false;
+        this.router.navigate(['/sauces']);
+      }
+    ).catch(
+      (error) => {
+        this.loading = false;
+        this.errorMessage = error.message;
+        console.error(error);
+      }
+    );
+  }
+} */
--- routes/sauce.js
+++ routes/sauce.js
@@ -5,7 +5,8 @@ const sauceController = require('../controllers/sauce');
 const auth = require('../middleware/auth');
 const multer = require('../middleware/multer-config');
 
-
+router.put ('/:id', auth, sauceController.dislikeSauce) 
+router.put ('/:id', auth, sauceController.likeSauce) 
 router.post('/', auth, multer,  sauceController.createSauce);
 router.get('/', auth, sauceController.getAllSauces);
 router.get('/:id', auth, sauceController.getOneSauce);
